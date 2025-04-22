07d51f790b04373f79f628e48692bc7aed85ddff
Fabien Toune <github@lapin-blanc.com>
2025-04-22 13:23:23 +0000

    mise à jour pour packaging

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='pyetnic',
     version='0.0.3',
-    packages=find_packages(),  # Détecte automatiquement tous les packages à inclure
-    include_package_data=True,  # Inclut les données non-code spécifiées dans MANIFEST.in
+    packages=find_packages(),  # Automatically find packages in the directory
+    include_package_data=True,  # Include non-code files specified in MANIFEST.in
     package_data={
         'pyetnic': [
-            'resources/*',
+            'resources/*',  # Include all files in the resources directory
         ],
     },
     install_requires=[
-        # Liste des dépendances nécessaires à installer
         'zeep',
         'python-dotenv',
+        # Add other dependencies here
     ],
     author='Fabien Toune',
     author_email='fabien.toune@eica.be',
@@ -21,4 +21,15 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Lapin-Blanc/pyetnic',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify the minimum Python version
+    entry_points={
+        'console_scripts': [
+            'pyetnic=pyetnic.cli:main',  # Command line interface entry point
+        ],
+    },
 )
