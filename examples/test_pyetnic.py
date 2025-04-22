d2febfe545dfa5f2d41f8dde954d1f3afb493ecb
Fabien Toune <github@lapin-blanc.com>
2025-04-22 13:16:16 +0000

    déplacement de l'exemple dans examples

--- examples/test_pyetnic.py
+++ examples/test_pyetnic.py
@@ -1,6 +1,12 @@
 """
 Script de test pour la bibliothèque pyetnic.
 """
+import sys
+import os
+
+# Add the root directory of the project to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import logging
 import json
 from pprint import pprint
